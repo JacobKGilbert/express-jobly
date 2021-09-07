134d2130b6714a56df8d25990bcd02a8f6785688
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-07 16:41:36 -0500

    added tests for findAll with filter

--- models/company.test.js
+++ models/company.test.js
@@ -85,6 +85,62 @@ describe("findAll", function () {
       },
     ]);
   });
+
+  test('works: with filter: minEmployees', async () => {
+    let filterData = { minEmployees: 2 }
+    let companies = await Company.findAll(filterData)
+    expect(companies).toEqual([
+      {
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img",
+      },
+      {
+        handle: "c3",
+        name: "C3",
+        description: "Desc3",
+        numEmployees: 3,
+        logoUrl: "http://c3.img",
+      },
+    ])
+  })
+
+  test('works: with filter: maxEmployees', async () => {
+    let filterData = { maxEmployees: 2 }
+    let companies = await Company.findAll(filterData)
+    expect(companies).toEqual([
+      {
+        handle: 'c1',
+        name: 'C1',
+        description: 'Desc1',
+        numEmployees: 1,
+        logoUrl: 'http://c1.img',
+      },
+      {
+        handle: 'c2',
+        name: 'C2',
+        description: 'Desc2',
+        numEmployees: 2,
+        logoUrl: 'http://c2.img',
+      },
+    ])
+  })
+
+  test('works: with filter: name', async () => {
+    let filterData = { name: 'C1' }
+    let companies = await Company.findAll(filterData)
+    expect(companies).toEqual([
+      {
+        handle: 'c1',
+        name: 'C1',
+        description: 'Desc1',
+        numEmployees: 1,
+        logoUrl: 'http://c1.img',
+      },
+    ])
+  })
 });
 
 /************************************** get */
