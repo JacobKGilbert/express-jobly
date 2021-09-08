0d756b4cbe0cb740c7a40d9555d2ed171a5bb536
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-08 15:44:48 -0500

    added sqlJobFilter

--- helpers/sql.js
+++ helpers/sql.js
@@ -51,4 +51,37 @@ function sqlCompanyFilter(filterData) {
   }
 }
 
-module.exports = { sqlForPartialUpdate, sqlCompanyFilter };
+/** Given filterData return formatted string for WHERE statement in SQL query */
+function sqlJobFilter(filterData) {
+  // hasEquity MUST BE LAST!!!
+  // {title: 'Comp', minSalary: 50000, hasEquity: true} =>
+  // ['"TITLE" LIKE "%$1%"', '"salary">=$2', '"equity">0']
+  const keys = Object.keys(filterData)
+  const values = Object.values(filterData)
+
+  const cols = keys.map((colName, idx) => {
+    if (colName === 'minSalary') {
+      return `"salary">=$${idx + 1}`
+    } else if (colName === 'title') {
+    return `upper(${colName}) LIKE upper('%' || $${idx + 1} || '%')`
+    } else if (colName === 'hasEquity') {
+      // We do not want a boolean in the values passed to the query. This will remove the boolean no matter true or false. 
+      values.splice(idx, 1)
+
+      if (filterData['hasEquity'] === true) {
+        return `"equity">0`
+      } else {
+        return ''
+      }
+    }
+  })
+
+  return {
+    // whereCriteria:
+    // '"NAME" LIKE "%$1%" AND "num_employees"<=$2 AND "num_employees">=$3'
+    whereCriteria: cols.join(' AND '),
+    values: values,
+  }
+}
+
+module.exports = { sqlForPartialUpdate, sqlCompanyFilter, sqlJobFilter };
